d699499eee08b99734d90e9d577543b536b1a33c
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-08-06 23:54:23 +0100

    Displaying results + styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html className="bg-slate-800" lang="en">
       <body className={inter.className}>
         <NavBar />
         {children}
